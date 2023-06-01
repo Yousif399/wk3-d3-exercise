6f75789fab7e536ffdd654d14f85e26acb67ec16
Jposeph99 <joseph.ahmed65@yahoo.com>
2023-06-01 16:08:58 -0600

    editiing done

--- exercise-dy3.py
+++ exercise-dy3.py
@@ -1,41 +1,41 @@
 #Exercise1
-# words = ['this' , 'is', 'a', 'sentence', '.']
+words = ['this' , 'is', 'a', 'sentence', '.']
 
-# def reverse(list_names,a,b,c,d,e):
-#     list_names[a],list_names[b],list_names[c],list_names[d],list_names[e],=list_names[e],list_names[d],list_names[c],list_names[b],list_names[a]
+def reverse(list_names,a,b,c,d,e):
+    list_names[a],list_names[b],list_names[c],list_names[d],list_names[e],=list_names[e],list_names[d],list_names[c],list_names[b],list_names[a]
 
-# reverse(words,4,3,2,1,0)
-# print(words)
+reverse(words,4,3,2,1,0)
+print(words)
 
 #Exercise2
-# a_text = 'In computing, a hash table hash map is a data structure which implements an associative array abstract data type, a structure that can map keys to values. A hash table uses a hash function to compute an index into an array of buckets or slots from which the desired value can be found'
-# b_text=" i am i am i am I AM"
-# def count_words(names):
-#     distinct_words={'i': 1}
-#     names = names.split()
-#     for name in names:
-#         name = name.lower()
-#         if name in distinct_words:
-#             distinct_words[name] += 1
-#         else:
-#             distinct_words[name] = 1
-#     return distinct_words
-# print(count_words(a_text))
-# print(count_words(b_text))
+a_text = 'In computing, a hash table hash map is a data structure which implements an associative array abstract data type, a structure that can map keys to values. A hash table uses a hash function to compute an index into an array of buckets or slots from which the desired value can be found'
+b_text=" i am i am i am I AM"
+def count_words(names):
+    distinct_words={'i': 1}
+    names = names.split()
+    for name in names:
+        name = name.lower()
+        if name in distinct_words:
+            distinct_words[name] += 1
+        else:
+            distinct_words[name] = 1
+    return distinct_words
+print(count_words(a_text))
+print(count_words(b_text))
 
 
 #Exercise3
-# def lin_search(list,y,x):
-#     for i in range(0,y):
-#         if list[i] == x:
-#             return f"answer found at index: {i}"
+def lin_search(list,y,x):
+    for i in range(0,y):
+        if list[i] == x:
+            return f"answer found at index: {i}"
         
-#     return 'answer is not founded'
-# list =[1,4,78,55,89,9,90,3]
-# x=55
-# y=len(list)
-# answer= lin_search(list,y,x)
-# print(answer)
+    return 'answer is not founded'
+list =[1,4,78,55,89,9,90,3]
+x=55
+y=len(list)
+answer= lin_search(list,y,x)
+print(answer)
 
 #Exercise3-using binary search
 def lin_search2(list,x):
